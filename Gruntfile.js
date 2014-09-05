5e1208a39be93aaf603e89d6b52d7c89f5407cdf
Gregor Martynus <gregor@martynus.net>
2014-09-05 15:26:52 +0200

    chore: initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-semantic-release');
+
+  grunt.initConfig({
+    release: {
+      email: 'gregor@martynus.net',
+      name: 'Gregor Martynus',
+    }
+  })
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Release Test
+============
+
+This is just a dummy repository
+to test generic release task.
